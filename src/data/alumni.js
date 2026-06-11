8c8e898b8aff453100dffdc620dde80f98c8f6d1
javasaigon75 <113626073+javasaigon75@users.noreply.github.com>
2026-06-10 20:31:50 -0400

    Add Trinh Buu Son to alumni roster

--- src/data/alumni.js
+++ src/data/alumni.js
@@ -101,6 +101,7 @@ const RAW = [
   ['Banh Dinh Tuan', 'Paris', 'France'],
   ['Le Van Anh', 'Paris', 'France'],
   ['Huynh Ngoc Minh', 'Paris', 'France'],
+  ['Trinh Buu Son', 'Portland, OR'],
   ]
 
 export const alumni = RAW.map(([name, location, country = 'USA'], i) => ({
